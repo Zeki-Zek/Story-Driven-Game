e83d5a110d919c5a354af6180ff9cc5b8d1be196
Zeki <zekiiipoesia@yahoo.com>
2025-05-15 10:05:30 +0800

    fixed the fade animation

--- Assets/Scripts/SceneTransitions/SceneChanger.cs
+++ Assets/Scripts/SceneTransitions/SceneChanger.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Collections;
 public class SceneChanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public string sceneToLoad;
+    public Animator transAnimation;
+
+    public float delayBeforeSceneLoad;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(sceneToLoad);
+
+            StartCoroutine(EnterCarpetAndLoadScene(collision.gameObject));
         }
+    }
+
+    IEnumerator EnterCarpetAndLoadScene(GameObject player)
+    {
+        transAnimation.SetTrigger("End");
+        yield return new WaitForSeconds(delayBeforeSceneLoad);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
