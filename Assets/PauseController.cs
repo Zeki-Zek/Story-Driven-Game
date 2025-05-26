7da8b5cff9568e5b4ea593e22fdc3bce73b44e89
Zeki <zekiiipoesia@yahoo.com>
2025-05-26 22:38:03 +0800

    PauseMenu Done

--- Assets/PauseController.cs
+++ Assets/PauseController.cs
@@ -2,9 +2,48 @@ using UnityEngine;
 
 public class PauseController : MonoBehaviour
 {
-    public static bool isGamePaused { get; private set; } = false;
-    public static void SetPause (bool pause)
+    /* public static bool isGamePaused { get; private set; } = false;
+     public static void SetPause (bool pause)
+     {
+         isGamePaused = pause;
+     }*/
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+    public static bool isGamePaused;
+    void Start()
     {
-        isGamePaused = pause;
+        pauseMenu.SetActive(false);
+        isGamePaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isGamePaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        isGamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        isGamePaused = false;
+        Time.timeScale = 1f;
     }
 }
