fd0d2222dc6c5039be49455ddaeb6a67f901bab7
Zeki <zekiiipoesia@yahoo.com>
2025-06-01 02:41:47 +0800

    still maps ongoing

--- Assets/TouchGrass.cs
+++ Assets/TouchGrass.cs
@@ -1,38 +1,43 @@
-using System.Collections;
 using UnityEngine;
-
-
 
 public class TouchGrass : MonoBehaviour
 {
+    private Animator grassTouch;
+    private bool isPlayerEntering = false;
+    private SpriteRenderer grassRenderer;
 
-    public Animator grassAnim1;
-    public Animator grassAnim2;
-    public Animator grassAnim3;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private string grassAnimState = "GrassIdle"; // Set your actual animation state name here
+
     void Start()
     {
-        
+        grassTouch = GetComponent<Animator>();
+        grassRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isPlayerEntering)
         {
+            isPlayerEntering = true;
+            // Force the animation to play from the beginning
+            grassTouch.SetTrigger("Touched");
+            grassTouch.SetTrigger("Step");
+            grassRenderer.sortingLayerName = "WalkBehind";
 
-            
-            grassAnim1.SetTrigger("Touched");
-            grassAnim2.SetTrigger("Touched");
-            grassAnim3.SetTrigger("Touched");
-            
 
 
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerEntering = false;
+            grassTouch.Play(grassAnimState, 0, 0f);
+            grassRenderer.sortingLayerName = "WalkInFront";
+
+        }
     }
 }
